e278325bec5457c43154c5977cf7aa06e94c2d37
DannnB <dbroughton_09@hotmail.com>
2018-12-02 01:00:43 +0000

    init nuxt app

---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <section class="container">
+    <div>
+      <h1 class="title">
+        DanB Designs
+      </h1>
+      <h2 class="subtitle">
+        New site on its way!
+      </h2>
+    </div>
+  </section>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+
+export default {
+  components: {
+    Logo
+  }
+}
+</script>
+
+<style>
+</style>
